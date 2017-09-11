ea0c60c75205c5b11ce5e4bcebb448fb25821aef
abdo farag <abdofarag85@gmail.com>
2017-09-11 05:38:30 +0200

    Add files via upload

---
+++ libs/nova-live-migration.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Unattended/SemiAutomatted OpenStack Installer
+# Abdo Farag.
+# E-Mail: abdofarag85@Gmail.com
+# OpenStack OCATA for Centos 7
+#
+## This script configures Nova to perform live migration option.
+#
+
+source $keystone_fulladmin_rc_file
+
+usermod -s /bin/bash nova  > /dev/null 2>&1
+echo "$novapass" | passwd nova --stdin > /dev/null 2>&1
+rm -rf /var/lib/nova/.ssh/id_rsa*
+sudo -u nova ssh-keygen -q -N "" -f /var/lib/nova/.ssh/id_rsa  > /dev/null 2>&1
+host=`hostname`
+novahost=`nova service-list | awk -F " " '{print $6}' |grep -v "Host" | grep -Ev "^$" | awk '!seen[$0]++'| grep -v "$host"`
+for i in `echo -e "$novahost"`
+do
+sshpass -p $novapass ssh-copy-id -o StrictHostKeyChecking=no -i /var/lib/nova/.ssh/id_rsa.pub nova@$i  > /dev/null 2>&1
+done
+
+echo "Host *" > /var/lib/nova/.ssh/config
+echo "StrictHostKeyChecking no" >> /var/lib/nova/.ssh/config
+echo "UserKnownHostsFile=/dev/null" >> /var/lib/nova/.ssh/config
+
